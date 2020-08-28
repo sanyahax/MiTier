62d98d94f446d49ec601255340c3f9e8a445fc02
sanyhax <alex@alexanderklemt.com>
2020-08-28 22:53:41 +0200

    voll lash

--- MiTier/ViewController.swift
+++ MiTier/ViewController.swift
@@ -22,15 +22,80 @@ extension UIView {
     }
 }
 
+extension ViewController: CBCentralManagerDelegate, CBPeripheralDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        let services = [CBUUID(string: "2c00")]
+        let cmdchar = [CBUUID(string: "2c10")]
+        switch central.state {
+            
+        case .unknown:
+            print("CB.state is unknown")
+        case .resetting:
+            print("CB.state is reset")
+        case .unsupported:
+            print("CB.state is unsupported")
+        case .unauthorized:
+            print("CB.state is unauth")
+        case .poweredOff:
+            print("CB.state is off")
+        case .poweredOn:
+            print("CB.state is on")
+            centralManager.scanForPeripherals(withServices: services)
+        }
+        
+    }
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
+        print(peripheral)
+        vehiclePeripheral = peripheral
+        centralManager.stopScan()
+        centralManager.connect(vehiclePeripheral)
+        vehiclePeripheral.delegate = self
+        print(vehiclePeripheral)
+    }
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+      print("Connected!")
+      print(vehiclePeripheral)
+        vehiclePeripheral.discoverServices([CBUUID(string: "2c00")])
+    }
+    
+
+
+
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else { return }
+        for service in services {
+          print(service)
+          print(service.characteristics ?? "characteristics are nil")
+          peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService,
+                    error: Error?) {
+      guard let characteristics = service.characteristics else { return }
+      for characteristic in characteristics {
+        print(characteristic)
+        vehicleCharacteristic = characteristic
+        
+      }
+    }
+}
+
+
 class ViewController: UIViewController {
 
     
-    
+    var centralManager: CBCentralManager!
+    var vehiclePeripheral: CBPeripheral!
+    var vehicleCharacteristic:CBCharacteristic!
     var menu: SideMenuNavigationController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        
         
         menu = SideMenuNavigationController(rootViewController: MenuListController())
         menu?.leftSide = true
@@ -53,17 +118,21 @@ class ViewController: UIViewController {
         if !enabled {
             sender.setImage(on_image, for: .normal)
             enabled = true
+            print("Vehicle unlocked.")
+            vehiclePeripheral.writeValue("AT+BKSCT=PASS123,1$,r,n".data(using: String.Encoding.utf8)!, for: vehicleCharacteristic, type: CBCharacteristicWriteType.withResponse)
+            
         }
         else if enabled {
             sender.setImage(off_image, for: .normal)
             enabled = false
+            print("Vehicle locked")
         }
         
     }
     @IBAction func didSlideSpeed(sender: UISlider) {
         let sliderThumb = UIImage(named: "arrow.left.and.right.square.fill")
         sender.setThumbImage(sliderThumb, for: UIControl.State.normal)
-        var speedInt:Int = Int(sender.value)
+        let speedInt:Int = Int(sender.value)
            print("Speed \(speedInt)")
     }
 }
@@ -71,6 +140,7 @@ class MenuListController: UITableViewController {
     var items = ["Change Password"]
         
     let darkColor = UIColor(red: 33/255.0, green: 33/255.0, blue: 33/255.0, alpha: 1)
+    let tierColor = UIColor(red: 106/255.0, green: 209/255.0, blue: 170/255.0, alpha: 1)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -85,9 +155,15 @@ class MenuListController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = items[indexPath.row]
-        cell.textLabel?.textColor = .white
+        cell.textLabel?.textColor = .black
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 25)
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.highlightedTextColor = .white
         
-        cell.backgroundColor = darkColor
+        self.tableView.separatorStyle = .none
+        self.tableView.isScrollEnabled = false
+        self.tableView.tableHeaderView?.backgroundColor = tierColor
+        cell.backgroundColor = tierColor
         return cell
     }
     
